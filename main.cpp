ad0b2b433f5972f9f7a6d2c02c55dff6ac1ab641
Dylan Palmquist <111770130+dmpalmquist@users.noreply.github.com>
2022-11-02 01:30:11 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string> 
+
+using namespace std;
+
+
+int main() {
+
+  string month, day, year;
+  int firsthour, firstmin, sechour, secmin;
+
+
+  while (cin >> month && cin >> day && cin >> year){
+
+    int THPD = 0;
+    int TMPD = 0;
+
+    cin >> firsthour;
+    cin.ignore();
+    cin >> firstmin;
+
+    cin >> sechour;
+    cin.ignore();
+    cin >> secmin;
+  }
+}
